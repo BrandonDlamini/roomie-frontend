6482f8deb8b265d4f7ed035c137c661a0aed0b76
Brandon <brandon.dlamini95@gmail.com>
2021-11-11 14:55:09 +0200

    fixed main page

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,15 @@ function App() {
   return (
     <div class="bg-gray-50">
       <Navbar />
+      <div className="pt-36 ">
+        <h1 className="font-mont text-4xl text-center font-bold text-gray-600 ">
+          WELCOME TO ROOMIE
+        </h1>
+        <p className="font-mont font-medium text-sm text-center text-gray-600">
+          {" "}
+          The platform for finding your next roomate
+        </p>
+      </div>
     </div>
   );
 }
