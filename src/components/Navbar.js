61946f341f1485cce8a8bddf13d9ed061295f4b5
Brandon <brandon.dlamini95@gmail.com>
2021-11-11 14:34:39 +0200

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-8 bg-purple-800 text-gray-300 items-center flex flex-row justify-between">
-      <div className="brand-logo text-sm font-bold">Roomie</div>
-      <ul className="menu-list flex flex-row text-xs font-bold">
+    <div className="fixed w-full h-14 bg-purple-800 text-gray-300 items-center flex flex-row justify-between">
+      <div className="brand-logo px-4 text-sm font-bold font-fontFamily font-mont">
+        Roomie
+      </div>
+      <ul className="menu-list flex flex-row text-xs font-bold font-fontFamily font-sans">
         <li className="menu-list-item px-2">
           <a href="#">Home</a>
         </li>
