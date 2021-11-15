b922ce00bfd6b6c8353d12286dc6c060a17a34d0
Brandon <brandon.dlamini95@gmail.com>
2021-11-15 20:54:06 +0200

    fixed Roomie component

--- src/components/Roomie.js
+++ src/components/Roomie.js
@@ -3,6 +3,16 @@ import React from "react";
 const Roomie = () => {
   return (
     <div>
+      <div>picture</div>
+      <div>
+        <div>
+          <button></button>
+        </div>
+        <div>
+          <p>Name:</p>
+          <p>Area:</p>
+        </div>
+      </div>
       <p>somthing</p>
     </div>
   );
