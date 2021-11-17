853f3376ef96f98c53aa4dcb94b0c2f3617f2766
Brandon <brandon.dlamini95@gmail.com>
2021-11-17 12:52:14 +0200

    added proptypes to roomie component

--- src/components/Roomie.js
+++ src/components/Roomie.js
@@ -1,7 +1,8 @@
 import React from "react";
 import SmallButton from "./Buttons/SmallButton";
+import PropTypes from "prop-types";
 
-const Roomie = () => {
+const Roomie = (name, area) => {
   return (
     <div className="shadow-md max-w-sm">
       <img
@@ -11,8 +12,10 @@ const Roomie = () => {
       />
       <div className="py-5 grid grid-cols-3 gap-4 bg-gray-100">
         <div className=" text-xs col-span-2 w-auto  font-mont font-medium  pl-4">
-          <p className="text-sm">Brandon Denvers </p>
-          <p className="text-rmPurple font-bold text-sm">Area: Hatfield</p>
+          <p className="text-sm">{name}Brandon Denvers </p>
+          <p className="text-rmPurple font-bold text-sm">
+            Area:{area} Hatfield
+          </p>
         </div>
         <div className=" pt-1 pr-4 justify-items-end ">
           <SmallButton />
@@ -22,5 +25,9 @@ const Roomie = () => {
   );
 };
 
+Roomie.propTypes = {
+  name: PropTypes.string,
+  area: PropTypes.string,
+};
+
 export default Roomie;
-//py-4 flex flex-row gap-36
