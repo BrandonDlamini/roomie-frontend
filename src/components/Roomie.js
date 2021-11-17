4fe89df8c2d941e64570dd0ea729d045b87f9741
Brandon <brandon.dlamini95@gmail.com>
2021-11-17 12:15:55 +0200

    fixed roomie styling

--- src/components/Roomie.js
+++ src/components/Roomie.js
@@ -3,18 +3,18 @@ import SmallButton from "./Buttons/SmallButton";
 
 const Roomie = () => {
   return (
-    <div className="border-4 border-black  max-w-sm">
+    <div className="shadow-md max-w-sm">
       <img
         src="https://i.ibb.co/5RvVnkL/3.jpg"
         alt="Roomate"
         className="h-1/6 w-full object-cover"
       />
-      <div className=" py-4 grid grid-cols-3 border-4 gap-4  border-yellow-500">
-        <div className=" text-xs col-span-2 w-auto  font-mont font-medium ml-2">
-          <p className="font-reg">Brandon Denvers Wayne</p>
-          <p className="font-bold text-sm">Area: Hatfield</p>
+      <div className="py-5 grid grid-cols-3 gap-4 bg-gray-100">
+        <div className=" text-xs col-span-2 w-auto  font-mont font-medium  pl-4">
+          <p className="text-sm">Brandon Denvers </p>
+          <p className="text-rmPurple font-bold text-sm">Area: Hatfield</p>
         </div>
-        <div className=" mr-4 justify-items-end">
+        <div className=" pt-1 pr-4 justify-items-end ">
           <SmallButton />
         </div>
       </div>
