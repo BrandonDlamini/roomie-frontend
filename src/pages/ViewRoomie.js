2bc6bf74e86e65a2e5b006d4fbb810f8c9d14e1f
Brandon <brandon.dlamini95@gmail.com>
2021-11-18 21:40:30 +0200

    added view roomie component

---
+++ src/pages/ViewRoomie.js
@@ -0,0 +1,12 @@
+import react from "react";
+import Navbar from "../components/Navbar";
+
+const ViewRoomie = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default ViewRoomie;
