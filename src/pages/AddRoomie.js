e827f3842f096ad26e71402d5200095f64e209aa
Brandon <brandon.dlamini95@gmail.com>
2021-12-20 22:06:47 +0200

    tweekd Addroomie component

--- src/pages/AddRoomie.js
+++ src/pages/AddRoomie.js
@@ -1,5 +1,6 @@
 import react from "react";
 import Welcome from "../components/typography/Welcome";
 import { useMutation } from "@apollo/react-hooks";
+import { ADD_ROOMIE } from "../graphql/Mutations";
 
 const AddRoomie = (props) => {};
